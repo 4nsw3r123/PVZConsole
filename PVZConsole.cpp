864849720bcc3736fa1c33bd1bd95a2f30f1a4a5
4nsw3r123 <2239888030@qq.com>
2021-12-17 15:06:52 +0800

    remove code about CTF

--- PVZConsole.cpp
+++ PVZConsole.cpp
@@ -36,9 +36,7 @@ int zombies[][3] =
 	{4400,0,0}, {4401,1,0},	{4402,2,0},	{4403,3,0},	{4404,4,0},	{4405,5,0},
 	{4800,2,3}, {4801,4,4}
 };
-//char flag[] = "aGFwcHl7Q29uNW8xZV9QVlpfMXNfdmVyeV8xbnQzcmU1dDFuZyF9";
-char flag[] = "aGFwcHl7dGhlX3NlY29uZF9mbGFnIWRvX3lvdV9rbm93X1RMUz99";
-int key[] = { 0,0,0,0,0,0,0,0,53,117,81,25,22,100,118,20,3,100,0,36,12,42,73,11,47,31,8,8,45,58,4,15,61,101,84,14,6,56,104,8,1,0,108,2,60,117,20,56,15,3,127,0,0 };
+
 void initLine()
 {
 	setCursor(0, 0);
@@ -83,70 +81,7 @@ void cursorDraw()
 	std::cout<<"__________";
 	setColor(0xA, 0);
 }
-char* base64_decode(char* code)
-{
-	//根据base64表，以字符找到对应的十进制数据  
-	int table[] = { 0,0,0,0,0,0,0,0,0,0,0,0,
-			 0,0,0,0,0,0,0,0,0,0,0,0,
-			 0,0,0,0,0,0,0,0,0,0,0,0,
-			 0,0,0,0,0,0,0,62,0,0,0,
-			 63,52,53,54,55,56,57,58,
-			 59,60,61,0,0,0,0,0,0,0,0,
-			 1,2,3,4,5,6,7,8,9,10,11,12,
-			 13,14,15,16,17,18,19,20,21,
-			 22,23,24,25,0,0,0,0,0,0,26,
-			 27,28,29,30,31,32,33,34,35,
-			 36,37,38,39,40,41,42,43,44,
-			 45,46,47,48,49,50,51
-	};
-	long len;
-	long str_len;
-	char* res;
-	int i, j;
 
-	//计算解码后的字符串长度  
-	len = strlen(code);
-	//判断编码后的字符串后是否有=  
-	if (strstr(code, "=="))
-		str_len = len / 4 * 3 - 2;
-	else if (strstr(code, "="))
-		str_len = len / 4 * 3 - 1;
-	else
-		str_len = len / 4 * 3;
-
-	res = (char*)malloc(sizeof(unsigned char) * str_len + 1);
-	res[str_len] = '\0';
-
-	//以4个字符为一位进行解码  
-	for (i = 0, j = 0; i < len - 2; j += 3, i += 4)
-	{
-		res[j] = ((unsigned char)table[code[i]]) << 2 | (((unsigned char)table[code[i + 1]]) >> 4); //取出第一个字符对应base64表的十进制数的前6位与第二个字符对应base64表的十进制数的后2位进行组合  
-		res[j + 1] = (((unsigned char)table[code[i + 1]]) << 4) | (((unsigned char)table[code[i + 2]]) >> 2); //取出第二个字符对应base64表的十进制数的后4位与第三个字符对应bas464表的十进制数的后4位进行组合  
-		res[j + 2] = (((unsigned char)table[code[i + 2]]) << 6) | ((unsigned char)table[code[i + 3]]); //取出第三个字符对应base64表的十进制数的后2位与第4个字符进行组合  
-	}
-	return res;
-
-}
-void NTAPI tls_callback(PVOID DllHandle, DWORD dwReason, PVOID)
-{
-	if (dwReason == DLL_PROCESS_ATTACH)
-		for (int i = 0; i < 53; i++)
-		{
-			flag[i] ^= key[i];
-		}
-}
-
-#pragma comment (linker, "/INCLUDE:_tls_used")  // See p. 1 below
-#pragma comment (linker, "/INCLUDE:tls_callback_func")  // See p. 3 below
-
-
-// Explained in p. 3 below
-#pragma const_seg(".CRT$XLF")
-EXTERN_C const
-
-PIMAGE_TLS_CALLBACK tls_callback_func = tls_callback;
-
-#pragma const_seg()
 
 int loop()
 {
@@ -287,7 +222,6 @@ int loop()
 			"                              Y   O   O U   U       W W W  I  N N N  !\n"
 			"                              Y   O   O U   U       W W W  I  N  NN   \n"
 			"                              Y    OOO   UUU         W W  III N   N  !\n" << std::endl;
-		printf("                        This is your flag: %s", base64_decode(flag));;
 	}
 	return 0;
 }
